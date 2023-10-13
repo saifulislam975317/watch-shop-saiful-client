cb71ffc2ceaf922e1204d9fe438cf57054d3e621
saiful islam <saifulislam975317@gmail.com>
2023-10-13 12:02:48 +0600

    update info

--- src/components/pages/BestSellers/BestSellers.jsx
+++ src/components/pages/BestSellers/BestSellers.jsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from "react";
 import useProducts from "../../../hooks/useProducts";
 
 const BestSellers = () => {
   const [products] = useProducts();
+  const [displayData, setDisplayData] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+
+  useEffect(() => {
+    if (showAll) {
+      setDisplayData(products.slice(0, 6));
+    } else {
+      setDisplayData(products.slice(0, 3));
+    }
+  }, [products, showAll]);
 
   return (
     <div>
       <h1 className="text-bold text-2xl">Our Best Sellers</h1>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4">
-        {products.slice(0, 6).map((product) => (
+        {displayData.map((product) => (
           <div key={product._id} className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
               <img
@@ -18,11 +29,18 @@ const BestSellers = () => {
             </figure>
             <div className="card-body items-center text-center">
               <h2 className="card-title">{product.name}</h2>
-
               <p>{product.price}</p>
             </div>
           </div>
         ))}
+      </div>
+      <div className="flex justify-center">
+        <button
+          className="btn btn-warning mt-5"
+          onClick={() => setShowAll(!showAll)}
+        >
+          {showAll ? "Show Less" : "Show All"}
+        </button>
       </div>
     </div>
   );
